b2acb62fa60b4fc1eae9b9ead3cc5de919272f39
Niranjan Desai <desainiranjan8@gmail.com>
2024-07-20 22:27:07 +0530

    Code Refactor

---
+++ src/main/java/com/example/tour_ms/flight.java
@@ -0,0 +1,15 @@
+package com.example.tour_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class flight {
+
+    @GetMapping("/flight")
+    public String getData() {
+
+        return  "Please book your flight tickets from tour app on 10% discount" ;
+    }
+}
